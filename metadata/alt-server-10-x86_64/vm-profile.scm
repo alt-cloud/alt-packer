e1c12d683ed6506ecfac03fe98481f2fc397942e
Alexander Stepchenko <geochip@altlinux.org>
2024-11-22 17:40:01 +0300

    add alt-server-10.2 configuration

--- metadata/alt-server-10-x86_64/vm-profile.scm
+++ metadata/alt-server-10-x86_64/vm-profile.scm
@@ -1,8 +1,9 @@
 ((server
   (title . "Setup for server")
   (action . trivial)
-  (actiondata 
+  (actiondata
                ; Disk size in sectors
                ; Calculating: disk_size (M) * 1024 * 2 - 4096
-               ("/" (size 61435904 . #t ) (fsim . "Ext2/3") (methods plain))
+               ; 5 GiB
+               ("/" (size 10481664 . #t ) (fsim . "Ext2/3") (methods plain))
                )))
