2ed700fa7e0524721fb4e25da8d713e1b4d162a3
Nadezhda Fedorova <fedor@altlinux.org>
2025-03-14 12:44:38 +0300

    Add efi boot to qcow2 images building, fix boot in configs of server c10f2, 10.4 and workstation 10.4

--- metadata/alt-server-10.4-x86_64/vm-profile.scm
+++ metadata/alt-server-10.4-x86_64/vm-profile.scm
@@ -3,6 +3,8 @@
   (action . trivial)
   (actiondata
                ; Disk size in sectors
+	       ; 10 MiB (for BIOS boot partition)
+               ("" (size 20480 . 20480) (methods plain))
                ; Calculating: disk_size (M) * 1024 * 2 - 4096
                ; 5 GiB
                ("/" (size 10481664 . #t ) (fsim . "Ext2/3") (methods plain))
