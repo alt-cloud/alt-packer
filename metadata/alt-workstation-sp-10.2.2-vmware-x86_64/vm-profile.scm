fc5f9350867fbf8acbe2167e3d7131fea8311697
Nadezhda Fedorova <fedor@altlinux.org>
2026-05-19 14:59:56 +0300

    Add configs for alt-server, alt-workstation images, format qcow2 and ova on c10f2 branch version 10.2.2 (sp)

---
+++ metadata/alt-workstation-sp-10.2.2-vmware-x86_64/vm-profile.scm
@@ -0,0 +1,12 @@
+((server
+  (title . "Setup for server")
+  (action . trivial)
+  (actiondata
+               ; Disk size in sectors
+               ; Calculating: disk_size (M) * 1024 * 2
+               ; 10 MiB (for BIOS boot partition)
+               ("" (size 20480 . 20480) (methods plain))
+               ; Calculating: disk_size (M) * 1024 * 2 - 4096
+               ; 12 GiB
+               ("/" (size 25161728 . #t) (fsim . "Ext2/3") (methods plain))
+               )))
