37fd96c9c6264e5ee8e819fd4f030653b9291eec
Nadezhda Fedorova <fedor@altlinux.org>
2026-05-22 11:56:39 +0300

    Add configs for alt-server, alt-workstation images, format qcow2 and ova on p11 branch version 11.1

---
+++ metadata/alt-server-11.1-vmware-x86_64/vm-profile.scm
@@ -0,0 +1,11 @@
+((server
+  (title . "Setup for server")
+  (action . trivial)
+  (actiondata
+               ; Disk size in sectors
+               ; 10 MiB (for BIOS boot partition)
+               ("" (size 20480 . 20480) (methods plain))
+               ; Calculating: disk_size (M) * 1024 * 2 - 4096
+	       ; 5 GiB
+               ("/" (size 10481664 . #t) (fsim . "Ext2/3") (methods plain))
+               )))
